fe0a0b1cccff4da732db5c716e887bced62424ad
Jones Nambundo <131800544+jonesnambundo@users.noreply.github.com>
2025-09-07 07:55:48 -0300

    Update Favorites.tsx

--- src/pages/Favoritos/Favorites.tsx
+++ src/pages/Favoritos/Favorites.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFavorites } from "../../hooks/useFavorites";
 import { fetchMovieDetails, fetchTvDetails } from "../../api/tmdbApi";
